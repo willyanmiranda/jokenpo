f71366533ff612266a7f969134c962615abc97d6
willyan miranda <willyanmiranda041@gmail.com>
2024-06-25 20:58:26 -0300

    first commit

---
+++ src/components/header/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Scoreboard from './scoreboard'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            Rock
+            Paper
+            Scissors
+        </div>
+
+        <Scoreboard/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/header/scoreboard/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Scoreboard = () => {
+  return (
+    <div>
+        <span>score</span>
+        <span>12</span>
+    </div>
+  )
+}
+
+export default Scoreboard
